ab5f1f8bcada38f4ad0d6e7dd156ffbcb3d1ce82
fariha6412 <52254116+fariha6412@users.noreply.github.com>
2020-06-23 15:25:18 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,16 @@
+n=int(input())
+while n:
+	newline=''
+	n-=1
+	maximum=0
+	line=input()
+	line=line.lower()
+	for c in "abcdefghijklmnopqrstuvwxyz":
+		k=line.count(c)
+		if k>=maximum:
+			maximum=k
+	for c in "abcdefghijklmnopqrstuvwxyz":
+		k=line.count(c)
+		if k==maximum:
+			newline+=c
+	print(newline)
